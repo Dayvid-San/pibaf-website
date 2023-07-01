774b2119820df49b7c876bab284bb554c55d4329
Dayvid Santana da Silva <santana.dayvid@outlook.com>
2023-07-01 15:27:59 -0300

    feat: Navbars component added

---
+++ src/components/navbars/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbars = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <img src="logo.png" alt="Logo" />
+      </div>
+      <div className="buttons">
+        {/* Aqui é onde vão ficar os botões */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbars;
