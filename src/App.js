d03faf3f7914f2861bf2841ec70492a6580ffb95
Replit user <>
2023-10-02 16:52:56 +0000

    feat: footer component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './Pages/home';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Home />
+      <Footer />
     </>
   );
 }
